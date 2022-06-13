c03054ae674a6420a272b4705f4c12305ea9743b
Zhanda <zh.zhanybek@gmail.com>
2022-06-13 08:56:47 +0600

    10.06

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
-; (() => {
 
-    const headerMenu = document.querySelector('.header-menu');
-    const menuIcon = headerMenu.querySelector('.header-menu__icon');
+const show_hide_password = (target) => {
+
+    const input = target.parentElement.querySelector('.form-input__input');
+
+    if (input.getAttribute("type") == "password") {
+        target.classList.add("view");
+        input.setAttribute("type", "text");
+    } else {
+        target.classList.remove("view");
+        input.setAttribute("type", "password");
+    }
+    input.focus();
+    return false;
+};
+
+
+(() => {
+    const headerMenu = document.querySelector(".header-menu");
+    const menuIcon = headerMenu.querySelector(".header-menu__icon");
 
     if (menuIcon) {
-        const menuList = headerMenu.querySelector('.header-menu__list');
-        headerMenu.addEventListener('click', (e) => {
-            menuIcon.classList.toggle('_active');
-            menuList.classList.toggle('_active')
-        })
+        const menuList = headerMenu.querySelector(".header-menu__list");
+        headerMenu.addEventListener("click", (e) => {
+            menuIcon.classList.toggle("_active");
+            menuList.classList.toggle("_active");
+        });
     }
-
 })();
 
 (() => {
-    const changeProfile = document.querySelector('.change-profile__block');
-    const changeProfileTitle = changeProfile.querySelector('.change-profile__title');
-    const changeProfileLabels = changeProfile.querySelectorAll('.change-profile__label');
+    const changeProfile = document.querySelector(".change-profile__block");
+    const changeProfileTitle = changeProfile.querySelector(
+        ".change-profile__title"
+    );
+    const changeProfileLabels = changeProfile.querySelectorAll(
+        ".change-profile__label"
+    );
     // Toggle menu
-    changeProfileTitle.addEventListener('click', () => {
-        if (changeProfile.dataset.state === 'active') {
-            changeProfile.setAttribute('data-state', '');
+    changeProfileTitle.addEventListener("click", () => {
+        if (changeProfile.dataset.state === "active") {
+            changeProfile.setAttribute("data-state", "");
         } else {
-            changeProfile.setAttribute('data-state', 'active');
+            changeProfile.setAttribute("data-state", "active");
         }
     });
     // Close when click to option
     for (let i = 0; i < changeProfileLabels.length; i++) {
-        changeProfileLabels[i].addEventListener('click', (evt) => {
-            changeProfile.setAttribute('data-state', '');
+        changeProfileLabels[i].addEventListener("click", (evt) => {
+            changeProfile.setAttribute("data-state", "");
         });
     }
-
 })();
 
 (() => {
-    const headerUser = document.querySelector('.header-user');
-    const userMenu = headerUser.querySelector('.header-user__menu');
-    const userMenuShowButton = headerUser.querySelector('.header-user__account');
+    const headerUser = document.querySelector(".header-user");
+    const userMenu = headerUser.querySelector(".header-user__menu");
+    const userMenuShowButton = headerUser.querySelector(
+        ".header-user__account"
+    );
 
     if (userMenu) {
-        userMenuShowButton.addEventListener('click', (evt) => {
-            userMenu.classList.toggle('_active');
+        userMenuShowButton.addEventListener("click", (evt) => {
+            userMenu.classList.toggle("_active");
         });
         // document.addEventListener('click', (evt) => {
 
@@ -52,30 +72,26 @@
         //     }
         // });
     }
-
 })();
 
 (() => {
     feather.replace();
 })();
 
-
-
 (() => {
     let pageName = document.location.pathname;
-    const pagesId = document.querySelectorAll('.header-menu__link');
-    pagesId.forEach(element => {
-        if (element.classList.contains('header-menu__link_active')) {
-            element.classList.remove('header-menu__link_active');
+    const pagesId = document.querySelectorAll(".header-menu__link");
+    pagesId.forEach((element) => {
+        if (element.classList.contains("header-menu__link_active")) {
+            element.classList.remove("header-menu__link_active");
         }
     });
-    pagesId.forEach(element => {
-        console.log(element.getAttribute('href').slice(1));
+    pagesId.forEach((element) => {
+        console.log(element.getAttribute("href").slice(1));
         console.log(pageName);
-        console.log(element.getAttribute('href').slice(1) === pageName);
-        if(element.getAttribute('href').slice(1) === pageName) {
-            element.classList.add('header-menu__link_active');
+        console.log(element.getAttribute("href").slice(1) === pageName);
+        if (element.getAttribute("href").slice(1) === pageName) {
+            element.classList.add("header-menu__link_active");
         }
     });
-
 })();
